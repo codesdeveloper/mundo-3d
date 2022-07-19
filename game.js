9f9d18749deae0a140df24ebce935d70e7cb7c61
Leandro Souza <leandros8w@gmail.com>
2022-07-19 22:05:00 +0000

    up

--- game.js
+++ game.js
@@ -16,7 +16,7 @@ scene.getCamera().setPosition(0, 2, -6);
 view.setFPS(60);
 view.setScene(scene);
 view.setCanvas(canvas);
-Util.addController(scene.getCamera(), canvas);
+//Util.addController(scene.getCamera(), canvas);
 //Definir objetos
 var cube = Textures.createCube();
 var pyramid = Textures.createPyramid();
@@ -29,13 +29,13 @@ pyramid.getStyle().color = [255, 255, 255];
 pyramid.getStyle().type = Entity.TYPEPOLYGONUS;
 cube2.setStyle({color: [0, 255, 0], size: 10, type: Entity.TYPEPOLYGONUS});
 cube2.getPolygonus()[0].color = [255, 0, 0];
-cube2.getStyle().color = [0, 0, 255];
+cube2.getStyle().color = [0, 0, 255];/*
 cube2.getPolygonus()[0].texture = "images/terra_lado.png";
 cube2.getPolygonus()[1].texture = "images/terra_lado.png";
 cube2.getPolygonus()[2].texture = "images/terra_lado.png";
 cube2.getPolygonus()[3].texture = "images/terra_lado.png";
 cube2.getPolygonus()[4].texture = "images/terra_cima.jpeg";
-cube2.getPolygonus()[5].texture = "images/terra_baixo.jpg";
+cube2.getPolygonus()[5].texture = "images/terra_baixo.jpg"; */
 //Adicionar itens no layout
 scene.addItem(plane, 0, -1, 0);
 scene.addItem(cube, 0, 0, 0);
@@ -51,8 +51,8 @@ scene.addItem(cube2.clone(), 4, 0, 10);
 scene.addItem(cube2.clone(), 6, 0, 8);
 //Start animação
 view.frameAnimation(() => {
-   scene.getCamera().translate(controllConfig.translate.x, controllConfig.translate.y, controllConfig.translate.z);
-   scene.getCamera().rotate(controllConfig.rotate.x, controllConfig.rotate.y, controllConfig.rotate.z);
+   //scene.getCamera().translate(controllConfig.translate.x, controllConfig.translate.y, controllConfig.translate.z);
+  // scene.getCamera().rotate(controllConfig.rotate.x, controllConfig.rotate.y, controllConfig.rotate.z);
   cube2.rotate(0.02, 0, 0)
   scene.render();
   view.render();
