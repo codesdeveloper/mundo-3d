103e60a4a120057a669d7b7f9bc6eda1bf37436d
leandros8w@gmail.com <leandros8w@gmail.com>
2022-07-22 19:07:10 -0300

    up

--- game.js
+++ game.js
@@ -8,7 +8,6 @@ const width = window.innerWidth,
 let canvas = document.getElementById("canvas");
 let scene = new Scene();
 let view = new View();
-var controll =  new Controller();
 //Configurações do game
 canvas.width = width;
 canvas.height = height;
@@ -30,23 +29,23 @@ pyramid.getStyle().color = [255, 255, 255];
 pyramid.getStyle().type = Entity.TYPEPOLYGONUS;
 cube2.setStyle({color: [0, 255, 0], size: 10, type: Entity.TYPEPOLYGONUS});
 cube2.getPolygonus()[0].color = [255, 0, 0];
-cube2.getStyle().color = [0, 0, 255];/*
+cube2.getStyle().color = [0, 0, 255];
 cube2.getPolygonus()[0].texture = "images/terra_lado.png";
 cube2.getPolygonus()[1].texture = "images/terra_lado.png";
 cube2.getPolygonus()[2].texture = "images/terra_lado.png";
 cube2.getPolygonus()[3].texture = "images/terra_lado.png";
 cube2.getPolygonus()[4].texture = "images/terra_cima.jpeg";
-cube2.getPolygonus()[5].texture = "images/terra_baixo.jpg"; */
+cube2.getPolygonus()[5].texture = "images/terra_baixo.jpg"; 
 //Adicionar itens no layout
 scene.addItem(plane, 0, -1, 0);
-scene.addItem(cube, 0, 0, 0);
+scene.addItem(cube2, 0, 0, 0);
 scene.addItem(cube.clone(), -7, 0, 0);
 scene.addItem(pyramid, 7, 0, 0);
 //Start animação
 view.frameAnimation(() => {
    //scene.getCamera().translate(controllConfig.translate.x, controllConfig.translate.y, controllConfig.translate.z);
   // scene.getCamera().rotate(controllConfig.rotate.x, controllConfig.rotate.y, controllConfig.rotate.z);
-  cube2.rotate(0.02, 0, 0)
+  cube2.rotate(0.02, 0, 0);
   scene.render();
   view.render();
 });
